7ae73403a5e5a8ab3c6320405a1f65867f158a63
Thang <thangducvu777@gmail.com>
2023-01-12 16:35:09 -0600

    nav-bar init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TypographyComponent } from './typography/typography.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'typography', component: TypographyComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TypographyComponent } from './typography/typography.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypographyComponent
   ],
   imports: [
     BrowserModule,
